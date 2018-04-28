7c6f4021a23c3bb62b892693b27d236df8230663
Brandon McFarland <branmcf@gmail.com>
2018-04-28 17:36:08 -0500

    Db connection work

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const app = express();
+const connectionString = 'postgres://ubfshvpvonrkvr:b87b3d8160d836b9fac7762fa942f46b7c70055867ff78b2e5b550ae78b68b97@ec2-54-163-240-54.compute-1.amazonaws.com:5432/daledlksliu340?ssl=true'
 
 const { Pool } = require('pg');
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: connectionString,
   ssl: true
 });
 
